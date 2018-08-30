7e35e865c1ed5ad62f79e1314e930c89e8d939d7
zunym <zunyinmon@zunyinmon.com>
2018-08-30 15:55:15 +0800

    Added Date Picker, Hint, Error

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,11 +4,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
 
 const MODULES = [
     MatFormFieldModule,MatInputModule,
     MatButtonModule,MatToolbarModule,
-    MatIconModule
+    MatIconModule,FlexLayoutModule,
+    MatDatepickerModule,MatMomentDateModule
 ]
 @NgModule({
 imports: MODULES,
